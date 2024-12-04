702c22d30704ebd1bb646ee9a4147f47c119a4a1
Abraão Santos <115851871+TechAbraao@users.noreply.github.com>
2024-12-04 17:10:42 -0300

    iniciando o projeto

---
+++ src/views/menu_opcoes.py
@@ -0,0 +1,15 @@
+def menu_opcoes():
+    menu = ''''
+    *======================================
+    * [1] - Cadastrar Produto              
+    * [2] - Consultar Produto              
+    * [3] - EM CONSTRUÇÃO
+    * [4] - EM CONSTRUÇÃO
+    * [5] - EM CONSTRUÇÃO
+    * [3] - EM CONSTRUÇÃO
+    *======================================
+    '''
+    print(menu)
+    opcao = int(input("Digite a opção: "))
+
+    return opcao
